34213d265a4fa18a888bd7fa12a803d8815a9845
Hamidreza Dadafarid <hamidreza.dadafarid@aut.ac.ir>
2024-11-27 22:50:09 +0330

    chore: minor change in question model

--- internal/domain/model/question.go
+++ internal/domain/model/question.go
@@ -1,23 +1,12 @@
 package model
 
-import (
-	"database/sql"
-	"time"
-
-	"github.com/google/uuid"
-)
-
-type BaseModel struct {
-	ID        uuid.UUID `gorm:"primarykey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt sql.NullTime `gorm:"index"`
-}
-
 type Questions struct {
-	BaseModel       `gorm:"embedded"`
-	Title           string         `gorm:"type:varchar;not null"`
-	Type            string         `gorm:"type:enum('multichoice','descriptive');not null"`
-	ShowIf          map[string]int `gorm:"type:json"`
-	QuestionnaireID uint           `gorm:"column:questionnaire_id;not null"`
+	BaseModel           `gorm:"embedded"`
+	Title               string `gorm:"type:varchar;not null"`
+	Type                string `gorm:"type:enum('multichoice','descriptive');not null"`
+	FilePath            string `gorm:"type:varchar;column:file_path"`
+	Order               int    `gorm:"type:int"`
+	QuestionnaireID     uint   `gorm:"column:questionnaire_id;not null"`
+	DependsOnQuestionId uint   `gorm:"column:depends_on_question_id"`
+	DependsOnOptionId   uint   `gorm:"column:depends_on_option_id"`
 }
