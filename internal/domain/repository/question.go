7fbd46b634833728a1a940a6d5914145c8dc8c54
Hamidreza Dadafarid <hamidreza.dadafarid@aut.ac.ir>
2024-11-27 20:53:05 +0330

    chore: minor changes in question repository

--- internal/domain/repository/question.go
+++ internal/domain/repository/question.go
@@ -6,10 +6,10 @@ import (
 )
 
 type IQuestionRepository interface {
-	Add(question model.Questions) (model.Questions, error)
-	Get(ids []uint) ([]model.Questions, error)
-	Update(question model.Questions) error
-	Delete(id uint) error
+	CreateQuestion(question model.Questions) error
+	GetQuestionById(ids []uint) (*model.Questions, error)
+	UpdateQuestion(question model.Questions) error
+	DeleteQuestion(id uint) error
 }
 
 type QuestionRepository struct {
@@ -21,23 +21,20 @@ func NewQuestionRpository(db *gorm.DB) IQuestionRepository {
 	return &QuestionRepository{db: db}
 }
 
-func (r *QuestionRepository) Add(question model.Questions) (model.Questions, error) {
-	result := r.db.Create(&question)
-	return question, result.Error
+func (r *QuestionRepository) CreateQuestion(question model.Questions) error {
+	return r.db.Create(&question).Error
 }
 
-func (r *QuestionRepository) Get(ids []uint) ([]model.Questions, error) {
-	var questions []model.Questions
-	result := r.db.Find(&questions, ids)
-	return questions, result.Error
+func (r *QuestionRepository) GetQuestionById(ids []uint) (*model.Questions, error) {
+	var question model.Questions
+	result := r.db.First(&question, ids)
+	return &question, result.Error
 }
 
-func (r *QuestionRepository) Update(question model.Questions) error {
-	result := r.db.Save(&question)
-	return result.Error
+func (r *QuestionRepository) UpdateQuestion(question model.Questions) error {
+	return r.db.Save(&question).Error
 }
 
-func (r *QuestionRepository) Delete(id uint) error {
-	result := r.db.Delete(&model.Questions{}, id)
-	return result.Error
+func (r *QuestionRepository) DeleteQuestion(id uint) error {
+	return r.db.Delete(&model.Questions{}, id).Error
 }
